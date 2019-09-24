682a296ca4604bbc9f94f44715531f8f2e5269e9
joshi51 <joshisuresh51@gmail.com>
2019-09-24 23:14:24 +0530

    - 'this' issue fixed

--- server/app/admin/movie/movie.controllers.ts
+++ server/app/admin/movie/movie.controllers.ts
@@ -5,7 +5,7 @@ import {MovieServices} from "./movie.services";
 @injectable()
 export class MovieControllers {
     constructor(@inject(Symbols.MovieServices) private movieServices: MovieServices){}
-    public async getMovieById(req, res){
+    getMovieById = async (req, res) => {
         try {
             let movie = await this.movieServices.getMovieById('5d89b6a6ecbd1509be2e8407');
             res.send(movie)
