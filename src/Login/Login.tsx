8bfeb896ed89f1772314b727f64e243339b965c5
joshi51 <joshisuresh51@gmail.com>
2021-04-26 22:47:23 +0530

    setup redux store dispatch

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import { Button, Container, FormControl, InputLabel, Input } from '@material-ui/core';
-import { connect } from 'react-redux';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import { connect, useSelector } from 'react-redux';
 import { UserService } from '../shared/services';
 import { userLogin } from '../shared/store/actions';
 import { get } from 'lodash';
+import './Login.scss';
+import {Redirect} from 'react-router-dom';
+
+const mapStateToProps = (state: any) => {
+  return state;
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    userLogin: (payload: any) => dispatch(userLogin(payload))
+  };
+};
 
 const userService = new UserService();
 
-class Login extends React.Component<{ userLogin: any }, { username: string, password: string }> {
+class Login extends React.Component< any , { username: string, password: string, redirectPath: string }> {
+  private storeData: any;
   constructor(props: any) {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      redirectPath: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  public componentDidUpdate() {
+    if (get(this.props, 'user.userData')) {
+      this.setState({...this.state, redirectPath: '/'});
+    }
+  }
+
+  public componentWillUnmount() {
+    this.setState = (state,callback)=>{
+      return;
+    };
   }
   
   public handleChange(event: any) {
@@ -40,30 +66,35 @@ class Login extends React.Component<{ userLogin: any }, { username: string, pass
       .then((res) => {
         const authResponse = get(res, 'data');
         if (authResponse) {
-          localStorage.setItem('auth', JSON.stringify(authResponse));
-          this.props.userLogin(authResponse);
+          sessionStorage.setItem('auth', JSON.stringify(authResponse.token.user));
+          this.props.userLogin(authResponse.token.user)
+          this.setState({...this.state, redirectPath: '/'});
         }
       })
       .catch((err) => console.log(err));
   }
   
   public render() {
-    return (
-      <Container>
-        <h5>Login Form</h5>
-          <FormControl fullWidth>
-            <InputLabel>Username</InputLabel>
-            <Input type="text" id="loginUsername" placeholder="Enter Username" value={this.state.username} onChange={this.handleChange}/>
-          </FormControl>
-          
-          <FormControl fullWidth>
-            <InputLabel>Password</InputLabel>
-            <Input type="password" id="loginPassword" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
-          </FormControl>
-          <Button variant="contained" color="primary" type="submit" onClick={this.handleSubmit}>Submit</Button>
-      </Container>
-    );
-  }
+    if (this.state.redirectPath) {
+      return <Redirect to={this.state.redirectPath} push/>;
+    } else {
+      return (
+        <div className="login-form">
+          <h5>Login Form</h5>
+            <FormControl fullWidth>
+              <InputLabel>Username</InputLabel>
+              <Input type="text" id="loginUsername" placeholder="Enter Username" value={this.state.username} onChange={this.handleChange}/>
+            </FormControl>
+            {this.storeData}
+            <FormControl fullWidth>
+              <InputLabel>Password</InputLabel>
+              <Input type="password" id="loginPassword" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
+            </FormControl>
+            <Button variant="contained" color="primary" type="submit" className="submit" onClick={this.handleSubmit}>Submit</Button>
+        </div>
+      );
+    }
+    }
 }
 
-export default connect(null, {userLogin})(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
